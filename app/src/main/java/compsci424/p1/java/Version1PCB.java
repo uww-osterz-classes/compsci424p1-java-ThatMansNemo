673e74ce3da8e28a2ca7972c51c62038ab298316
ThatMansNemo <159948611+ThatMansNemo@users.noreply.github.com>
2024-02-27 13:14:52 -0600

    Update Version1PCB.java

--- app/src/main/java/compsci424/p1/java/Version1PCB.java
+++ app/src/main/java/compsci424/p1/java/Version1PCB.java
@@ -1,12 +1,37 @@
-/* COMPSCI 424 Program 1
- * Name:
- */
-package compsci424.p1.java;
+import java.util.LinkedList;
 
-/**
- * The process control block structure that is used to track a
- * process's parent and children (if any) in Version 1.
- */
 public class Version1PCB {
+
+    Version1PCB parent;
+    LinkedList<Version1PCB> children;
+    int parentPid;
+
+   
+    public Version1PCB(Version1PCB parent, int parentPid) {
+        this.parent = parent;
+        this.children = new LinkedList<>();
+        this.parentPid = parentPid;
+    }
+
+
+    public void initializePCBArray(Version1PCB[] pcbArray, int parent) {
+        for (int i = 0; i < pcbArray.length; i++) {
+            pcbArray[i] = null;
+        }
+        int parentIndex = indexOf(pcbArray, parent);
+        if (parentIndex != -1 && pcbArray[parentIndex] == null) {
+            pcbArray[parentIndex] = this;
+        }
+    }
+
     
+    private int indexOf(Version1PCB[] pcbArray, int parent) {
+        for (int i = 0; i < pcbArray.length; i++) {
+            if (pcbArray[i] != null && pcbArray[i].parentPid == parent) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
+
