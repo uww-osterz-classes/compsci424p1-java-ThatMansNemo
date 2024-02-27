45b223132277c613be0de026d01635d564e1d7d8
ThatMansNemo <159948611+ThatMansNemo@users.noreply.github.com>
2024-02-27 13:15:54 -0600

    Update Program1.java

--- app/src/main/java/compsci424/p1/java/Program1.java
+++ app/src/main/java/compsci424/p1/java/Program1.java
@@ -7,7 +7,12 @@
  * classes, methods, and data structures that you need to solve the
  * problem and display your solution in the correct format.
  */
-package compsci424.p1.java;
+//package compsci424.p1.java;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+//import compsci424.p1.java.Version1;
 
 /**
  * Main class for this program. The required steps have been copied
@@ -22,42 +27,94 @@ public class Program1 {
      * @param args command-line arguments, which can be ignored
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Version1 v1 = new Version1();
+        while (true) {
+            System.out.println("choose between:create n, destroy n,end");
+            String inputString = sc.nextLine();
+            if (inputString.equalsIgnoreCase("end")) {
+                break;
 
-        // 1. Ask the user to enter commands of the form "create N",
-        //    "destroy N", or "end", where N is an integer between 0 
-        //    and 15.
+            } else if (inputString.equalsIgnoreCase("create N")) {
+                while (inputString.equalsIgnoreCase("create N") || inputString.equalsIgnoreCase("destroy n")) {
+                    System.out.println("enter a number 1-15");
+                    String input = sc.nextLine();
+                    if (input.equalsIgnoreCase("end")) {
+                        break;
+                    }
+                    if (input.equalsIgnoreCase("destroy n")) {
+                        break;
+                    }
+                    int parsedInput = Integer.parseInt(input);
+                    if (parsedInput < 0 || parsedInput > 15) {
+                        System.out.println("wrong number");
+                    }
+                    else{
+                    int parsedV1 = v1.create(parsedInput);
+                    v1.showProcessInfo();
+                    }
+                }
 
-        // 2. While the user has not typed "end", continue accepting
-        //    commands. Add each command to a list of actions to take 
-        //    while you run the simulation.
-        // 3. When the user types "end" (or optionally any word that 
-        //    is not "create" or "destroy"), stop accepting commands 
-        //    and complete the following steps.
-        //
-        // Hint: Steps 2 and 3 refer to the same loop. ;-)
-
-        // 4. Create an object of the Version 1 class and an object of
-        //    the Version 2 class.
-
-        // 5. Run the command sequence once with the Version 1 object, 
-        //    calling its showProcessTree method after each command to
-        //    show the changes in the tree after each command.
-
-        // 6. Repeat step 5, but with the Version 2 object.
-
-        // 7. Store the current system time in a variable
-
-        // ... then run the command sequence 200 times with Version 1.
-
-        // ... After this, store the new current system time in a
-        //     second variable. Subtract the start time from the end 
-        //     time to get the Version 1 running time, then display 
-        //     the Version 1 running time.
-
-        // 8. Repeat step 7, but with the Version 2 object.
-
-        // This line is here just to test the Gradle build procedure.
-        // You can delete it.
-        System.out.println("Builds without errors and runs to completion.");
+            } else if (inputString.equalsIgnoreCase("destroy n")) {
+                while (inputString.equalsIgnoreCase("create N") || inputString.equalsIgnoreCase("destroy n")) {
+                    System.out.println("enter a number 1-15");
+                    String input = sc.nextLine();
+                    if (input.equalsIgnoreCase("end")) {
+                        break;
+                    }
+                    if (input.equalsIgnoreCase("create n")) {
+                        break;
+                    }
+                    int parsedInput = Integer.parseInt(input);
+                    if (parsedInput < 0 || parsedInput > 16) {
+                        System.out.println("wrong number");
+                    }
+                    int parsedV1 = v1.destroy(parsedInput);
+              v1.showProcessInfo();
+                }
+            }
+        }
+        
     }
+    long startTimeV1 = System.currentTimeMillis();
+    long endTimeV1 = System.currentTimeMillis();
+        long runningTimeV1 = endTimeV1 - startTimeV1;
+        System.out.println("Version 1 running time: " + runningTimeV1 + " milliseconds.");
+
 }
+// 1. Ask the user to enter commands of the form "create N",
+// "destroy N", or "end", where N is an integer between 0
+// and 15.
+
+// 2. While the user has not typed "end", continue accepting
+// commands. Add each command to a list of actions to take
+// while you run the simulation.
+// 3. When the user types "end" (or optionally any word that
+// is not "create" or "destroy"), stop accepting commands
+// and complete the following steps.
+//
+// Hint: Steps 2 and 3 refer to the same loop. ;-)
+
+// 4. Create an object of the Version 1 class and an object of
+// the Version 2 class.
+
+// 5. Run the command sequence once with the Version 1 object,
+// calling its showProcessTree method after each command to
+// show the changes in the tree after each command.
+
+// 6. Repeat step 5, but with the Version 2 object.
+
+// 7. Store the current system time in a variable
+
+// ... then run the command sequence 200 times with Version 1.
+
+// ... After this, store the new current system time in a
+// second variable. Subtract the start time from the end
+// time to get the Version 1 running time, then display
+// the Version 1 running time.
+
+// 8. Repeat step 7, but with the Version 2 object.
+
+// This line is here just to test the Gradle build procedure.
+// You can delete it.
+// System.out.println("Builds without errors and runs to completion.");
