54b10703d633355ad5f0831509bfac52a0a190ac
ThatMansNemo <159948611+ThatMansNemo@users.noreply.github.com>
2024-02-28 16:19:05 -0600

    Update Version2PCB.java

--- app/src/main/java/compsci424/p1/java/Version2PCB.java
+++ app/src/main/java/compsci424/p1/java/Version2PCB.java
@@ -1,13 +1,13 @@
-/* COMPSCI 424 Program 1
- * Name:
- */
-package compsci424.p1.java;
-
-/**
- * The process control block structure that is used to track a
- * process's parent, first child, younger sibling, and older sibling
- * (if they exist) in Version 2.
- */
 public class Version2PCB {
-    
+	int parent;
+	int firstChild;
+	int youngerSibling;
+	int olderSibling;
+	
+	public Version2PCB(int parent) {
+		this.parent = parent;
+		this.firstChild = -1;
+		this.youngerSibling = -1;
+		this.olderSibling = -1;
+	}
 }
